bd1fc2252fecc9d0d6df4ab6a53fd834cc3d001d
CryptoverseGirl <kavithanarashiman2021@gmail.com>
2023-06-13 23:37:17 +0530

    ExportDatataskCompleted

--- Assets/Scripts/ExportDataManager.cs
+++ Assets/Scripts/ExportDataManager.cs
@@ -1,18 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using TMPro;
+using UnityEngine.Serialization;
 
 public class ExportDataManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+        public TextMeshProUGUI TotalSpawnedBoxesText, damagedBoxesText, perfectBoxesText;
+        public Button exportButton;
         
-    }
+        private void Start()
+        {
+                exportButton.onClick.AddListener(ExportDataToCSV);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        private void ExportDataToCSV()
+        {
+                string spawnedBoxesCount = TotalSpawnedBoxesText.text;
+                string damagedBoxesCount = damagedBoxesText.text;
+                string perfectBoxesCount = perfectBoxesText.text;
+
+
+                string[] data = new string[] { spawnedBoxesCount, damagedBoxesCount, perfectBoxesCount };
+
+                string header = "Spawned Boxes, Damaged Boxes, Perfect Boxes";
+
+                string fileName = "converyordata.csv";
+                string downloadsPath = GetDownloadsPath();
+                string filePath = Path.Combine(downloadsPath, fileName);
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                        writer.WriteLine(header);
+                        writer.WriteLine(string.Join(",", data));
+                }
+
+                Debug.Log("Data exported to CSV file: " + filePath);
+        }
+
+        private string GetDownloadsPath()
+        {
+                string downloadsPath = "";
+
+#if UNITY_EDITOR_WIN
+                downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                        "Downloads");
+#elif UNITY_STANDALONE_WIN
+        downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+#elif UNITY_EDITOR_OSX
+        downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+#elif UNITY_STANDALONE_OSX
+        downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+#elif UNITY_EDITOR_LINUX
+        downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+#elif UNITY_STANDALONE_LINUX
+        downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+#endif
+                return downloadsPath;
+        }
 }
+
+
+
+
